3bdebb5853619fea4b0be8af832f3bb385b2b832
Megharth <m.c.lakhataria@gmail.com>
2020-05-16 02:51:07 -0400

    prod build

---
+++ docs/precache-manifest.a95d26ebbae4c37ed97166fc45f9c5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdd996c3f84a88698dcc",
+    "url": "/css/app.0e3331b1.css"
+  },
+  {
+    "revision": "fdd996c3f84a88698dcc",
+    "url": "/js/app.593a452c.js"
+  },
+  {
+    "revision": "bef68413b4bbe2b472d0",
+    "url": "/css/chunk-vendors.eda8c5f9.css"
+  },
+  {
+    "revision": "bef68413b4bbe2b472d0",
+    "url": "/js/chunk-vendors.30b8cefa.js"
+  },
+  {
+    "revision": "e58377e2ec9f69d0816bc2f4c6205101",
+    "url": "/img/Megharth.e58377e2.png"
+  },
+  {
+    "revision": "6b32b682ddba7cb9aed915c174d72673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
